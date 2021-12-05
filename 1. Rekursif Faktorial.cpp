3336f152628726fa85b7043ed2d57d6ab034ebe5
AdityaPUPRA <90174892+AdityaPUPRA@users.noreply.github.com>
2021-12-05 21:19:27 +0700

    Add files via upload

---
+++ 1. Rekursif Faktorial.cpp
@@ -0,0 +1,21 @@
+/* Fungsi Rekursif Untuk Menghitung Nilai Faktorial */
+
+#include <iostream>
+
+using namespace std;
+
+long int faktorial (int A);
+int main (){
+	int r, hasil;
+	cout <<"MENGHITUNG NILAI FAKTORIAL DENGAN REKRUSIF"<<endl;
+	cout <<endl;
+	cout <<"Masukkan Nilai = "; cin >>r;
+	hasil=faktorial (r);
+	cout <<"Faktorial "<<r<<"!="<<hasil<<endl;
+}
+long int faktorial (int A){
+	if (A==1)
+	return (A);
+	else
+	return (A*faktorial(A-1));
+}
